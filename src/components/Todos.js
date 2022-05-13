251bc103048972614234880dbbd3ccaf44c0573c
tomaszaugustyniak <tomasz.augustyniak2000@gmail.com>
2022-05-13 12:22:06 +0200

    first commit

---
+++ src/components/Todos.js
@@ -0,0 +1,53 @@
+import React, {useRef, useState} from 'react'
+import { connect } from 'react-redux';
+import { addTodos, completeTodos, removeTodos, updateTodos } from '../redux/reducer';
+
+const mapStateToProps = (state) => {
+    return {
+        todos: state,
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        addTodo: (obj) => dispatch(addTodos(obj)),
+        removeTodo: (id) => dispatch(removeTodos(id)),
+        updateTodo: (obj) => dispatch(updateTodos(obj)),
+        completeTodo: (id) => dispatch(completeTodos(id)),
+    };
+};
+
+const Todos = (props) => {
+    const [todo, setTodo] = useState("");
+
+    const handleChange = (e) => {
+        setTodo(e.target.value);
+    };
+
+     //console.log("props from store", props);
+  return (
+    <div className='addTodos'>
+        <input 
+        type="text"
+         onChange={(e)=> handleChange(e)}
+          className="todo-input" />
+        
+        <button className="add-btn"
+        onClick={() =>
+        props.addTodo({
+            id:Math.floor(Math.random()* 1000),
+            item: todo,
+            completed: false,
+            })
+    }>
+        Add
+     </button>
+
+     <br/>
+     
+
+    </div>
+  )
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(Todos);
